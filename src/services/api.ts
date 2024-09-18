3dbcb04e8f85eecdfdc61b00fb02914b7ebbd665
Isabella Brito <isadocarmo7@gmail.com>
2024-09-18 19:10:02 -0300

    Criacao boilerplate frontend

---
+++ src/services/api.ts
@@ -0,0 +1,14 @@
+// import axios from 'axios';
+// import useAuth from '../hooks/useAuth';
+// const { handleGetToken } = useAuth();
+
+// const token = handleGetToken();
+
+// export const instance = axios.create({
+//   baseURL: '',
+//   timeout: 1000,
+//   headers: {
+//     'Content-Type': 'application/json',
+//     Authorization: `Bearer ${token}`,
+//   },
+// });
