d27f614c996a0960d12b5bc68c964234a6bb8c49
Isabella Brito <isadocarmo7@gmail.com>
2024-09-27 11:35:34 -0300

    wip: Finalização Layout Pagina SignIn

--- src/pages/SignIn/SignIn.tsx
+++ src/pages/SignIn/SignIn.tsx
@@ -4,6 +4,7 @@ import InputStd from '@/components/InputStd/InputStd';
 import style from './SignIn.module.css';
 import InputPassword from '@/components/InputPassword/InputPassword';
 import { useState } from 'react';
+import Logo from '../../assets/Logo.svg';
 
 function SignIn() {
   const [form, setForm] = useState({
@@ -22,8 +23,11 @@ function SignIn() {
 
   return (
     <div className={style.signIn_container}>
+      <img className={style.singIn_img} src={Logo} alt="Logo Move" />
+      <h3 className={style.singIn_input_title}>Entre na sua conta</h3>
       <InputStd placeholder="E-mail" />
       <InputPassword state={form.password} handleChange={handleChangeForm} />
+      <p className={style.singIn_input_text}>Esqueceu a senha ?</p>
       <ButtonStd name="Entrar" />
       <ButtonWhite name="Criar conta" />
     </div>
