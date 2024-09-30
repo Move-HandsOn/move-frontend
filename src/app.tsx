e20117f6d8df22917ca3a639ca70420f3602209d
ViniciusFialhus <contatoviniciusfialhu@gmail.com>
2024-09-30 14:53:12 -0300

    feat: ajustado o componente tabBar e ajustado ambientes de importação

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,16 @@
 import style from './app.module.css';
+import TabBar from './components/tabBar/tabBar';
+import Feed from './pages/Feed/Feed';
 import SignIn from './pages/SignIn/SignIn';
 
 function App() {
   return (
-    <div className={style.mainPage}>
-      <SignIn />
+    <div className={style.containerMain}>
+      <div className={style.main}>
+        <div className={style.header}></div>
+        <div className={style.content}></div>
+        <div className={style.footer}><TabBar/></div>
+      </div>
     </div>
   );
 }
