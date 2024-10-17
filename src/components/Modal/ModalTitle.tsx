4a98a89e16ba9c534ba433ef15bec118f64250bc
Amandalfs <amandalfs57@gmail.com>
2024-10-17 16:02:24 -0300

    fix: arrumado a estilizacao do h1 do modal geral

--- src/components/Modal/ModalTitle.tsx
+++ src/components/Modal/ModalTitle.tsx
@@ -1,11 +1,12 @@
-import style from './ModalRoot.module.css'; // Ajuste o caminho conforme necessário
+import style from './ModalRoot.module.css'; 
 
-type ModalTitleProps = React.HTMLProps<HTMLParagraphElement>
+type ModalTitleProps = React.HTMLProps<HTMLHeadingElement>;
 
-const ModalTitle = ({ children, ...rest}: ModalTitleProps) => {
-  return (
-    <div className={style.modalTitle} {...rest}>
+const ModalTitle = ({ children, ...rest }: ModalTitleProps) => {
+  return (<div className={style.modalTitle}>
+    <h1 {...rest}>
       {children}
+    </h1>
     </div>
   );
 };
