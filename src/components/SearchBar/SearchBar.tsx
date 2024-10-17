1440e856d667e171b565225fcf4bf70f2cb51281
Isabella Brito <isadocarmo7@gmail.com>
2024-10-16 23:00:19 -0300

    wip: criação componente SearchNav

---
+++ src/components/SearchBar/SearchBar.tsx
@@ -0,0 +1,54 @@
+import Paper from '@mui/material/Paper';
+import InputBase from '@mui/material/InputBase';
+import MagnifyingGlass from '../../assets/MagnifyingGlass.svg';
+
+export default function SearchBar() {
+  return (
+    <Paper
+      component="form"
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        flexDirection: 'row',
+        width: '324px',
+        height: '48px',
+        padding: '12px 16px',
+        borderRadius: '8px',
+        border: 'none',
+        boxShadow: 'none',
+        backgroundColor: '#E7E7E7',
+        gap: '23px',
+        color: '#1E1E1E',
+        position: 'absolute',
+        right: '15px',
+      }}
+    >
+      <img
+        src={MagnifyingGlass}
+        alt="Logo de busca"
+        style={{
+          width: '24px',
+          height: '24px',
+          background: '#E7E7E7',
+          cursor: 'pointer',
+        }}
+      />
+      <InputBase
+        sx={{
+          ml: 1,
+          flex: 1,
+          background: '#E7E7E7',
+          fontSize: '16px',
+          fontWeight: '700',
+          color: '#1E1E1E',
+          '& input::placeholder': {
+            color: '#1E1E1E',
+            opacity: 1,
+          },
+        }}
+        placeholder="Pesquisar"
+        inputProps={{ 'aria-label': 'search google maps' }}
+      />
+    </Paper>
+  );
+}
--- src/routes/MainRoutes.tsx
+++ src/routes/MainRoutes.tsx
@@ -4,6 +4,7 @@ import Feed from '../pages/Feed/Feed';
 import RequireAuth from './RequireAuth';
 import NewActivity from '../pages/NewActivity/NewActivity.tsx';
 import Layout from '@/layout.tsx';
+import NewPage from '@/pages/NewPage/NewPage.tsx';
 
 function MainRoutes() {
   return (
@@ -27,6 +28,15 @@ function MainRoutes() {
         element={
           <RequireAuth>
             <NewActivity />
+          </RequireAuth>
+        }
+      />
+
+      <Route
+        path="/new-page"
+        element={
+          <RequireAuth>
+            <NewPage />
           </RequireAuth>
         }
       />
