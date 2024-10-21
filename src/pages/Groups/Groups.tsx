34d207a3d1c7ae77d4d2b1387cf5f9a8c153598f
fernando <anjosdelacerda@gmail.com>
2024-10-21 07:38:17 -0300

    feat: criação do component RectangleGroup, que possui propriedades diferentes do componente GroupCard e que será usado na page de groups

--- src/pages/Groups/Groups.tsx
+++ src/pages/Groups/Groups.tsx
@@ -1,9 +1,17 @@
-// import groupData from '../../mocks/groupData.json';
+import groupData from '../../mocks/groupData.json';
 import style from '../Groups/Groups.module.css';
 import Button from '@/components/Button/Button';
 import SearchBar from '@/components/SearchBar/SearchBar';
+import RectangleGroup from '@/components/RectangleGroup/RectangleGroup';
+
 
 const Groups = () =>{
+    // const jsonList = groupData
+    // const list = JSON.parse(jsonList)
+    // const listTemp: TempListData =  list
+
+    const listTemp =  groupData;
+    const obj = listTemp[0]
 
     return(
         <section className={style.feed_container}>
@@ -20,6 +28,14 @@ const Groups = () =>{
             <div className={style.nav_container}>
                 <SearchBar />
             </div>
+
+            <RectangleGroup 
+            id={obj.id}
+            title={obj.name}
+            img={obj.image}
+            members={obj.members}
+            events={1}
+            />
 
         </section>
     )
