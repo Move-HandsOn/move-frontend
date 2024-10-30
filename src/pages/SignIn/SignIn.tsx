1c47021169a8f94fbadd5fd4c6be96e9018dc2cf
Amandalfs <amandalfs57@gmail.com>
2024-10-30 19:34:41 -0300

    fix: ajustado a para que o input e input de password receba o ref corretamente

--- src/pages/SignIn/SignIn.tsx
+++ src/pages/SignIn/SignIn.tsx
@@ -2,34 +2,31 @@ import Button from '@/components/Button/Button';
 import InputStd from '@/components/InputStd/InputStd';
 import style from './SignIn.module.css';
 import InputPassword from '@/components/InputPassword/InputPassword';
-import { useState } from 'react';
 import Logo from '../../assets/Logo.svg';
+import { useForm } from "react-hook-form";
 
 function SignIn() {
-  const [form, setForm] = useState({
-    email: '',
-    password: '',
+  const { register, handleSubmit } = useForm({
+    defaultValues: {
+      email: "",
+      password: "",
+    }
   });
 
-  // const [setError] = useState('');
-
-  function handleChangeForm(event: {
-    target: { name: string; value: string };
-  }) {
-    // setError('');
-    setForm({ ...form, [event.target.name]: event.target.value });
-  }
+  const onSubmit = data => {
+    console.log("Dados do formulário:", data);
+  };
 
   return (
-    <div className={style.signIn_container}>
+    <form onSubmit={handleSubmit(onSubmit)} className={style.signIn_container}>
       <img className={style.singIn_img} src={Logo} alt="Logo Move" />
       <h3 className={style.singIn_input_title}>Entre na sua conta</h3>
-      <InputStd placeholder="E-mail" />
-      <InputPassword state={form.password} handleChange={handleChangeForm} />
+      <InputStd placeholder="E-mail" {...register("email")}/>
+      <InputPassword {...register("password")} />
       <a className={style.singIn_input_text}>Esqueceu a senha ?</a>
-      <Button name="Entrar" variant="standard" />
+      <Button name="Entrar" variant="standard" type="submit" />
       <Button name="Criar conta" variant="gray" />
-    </div>
+    </form>
   );
 }
 
--- src/services/requests.ts
+++ src/services/requests.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { PostTypes } from '../types/postTypes';
+import { api } from './api';
 
 const req = axios.create({
   baseURL: 'https://jsonplaceholder.typicode.com',
@@ -14,3 +15,24 @@ export const getPost = async (id: number): Promise<PostTypes> => {
   const result = await req.get(`/posts/${id}`);
   return result.data;
 };
+
+
+interface RequestLogin {
+  email: string;
+  password: string;
+}
+
+interface ResponseLogin {
+  accessToken: string;
+  refreshToken: string;
+}
+
+
+export const Login = async ({ email, password }: RequestLogin): Promise<ResponseLogin> => {
+  const response = await api.post("/login", {email, password});
+
+  return {
+    accessToken: response.data.accessToken,
+    refreshToken: response.data.refreshToken,
+  }
+}
