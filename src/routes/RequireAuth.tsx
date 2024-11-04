e11f1a54cae153a0a1cbba1e8e76da3ddca3857b
Amandalfs <amandalfs57@gmail.com>
2024-11-04 19:29:02 -0300

    fix: ajustado o refresh do token

--- src/routes/RequireAuth.tsx
+++ src/routes/RequireAuth.tsx
@@ -1,9 +1,34 @@
 import { Navigate, Outlet } from 'react-router-dom';
 import { useCookies } from 'react-cookie';
+import { useMutation } from '@tanstack/react-query';
+import { refreshToken } from '@/services/requests';
+import { useEffect } from 'react';
 
 function RequireAuth() {
-  const [cookies] = useCookies(['token'])
+  const [cookies, setCookie, removeCookie] = useCookies(['token', 'refresh_token'])
   const isAuth = cookies.token;
+
+  const { mutateAsync: RefreshAsync }= useMutation({
+    mutationFn: () => refreshToken(cookies.refresh_token),
+    onSuccess: (newToken) => {
+      setCookie('token', newToken, {
+        path: '/',
+        maxAge: 300,
+        secure: true,
+        sameSite: 'strict',
+      });
+    },
+    onError: () => {
+      removeCookie('token', { path: '/' });
+      removeCookie('refresh_token', { path: '/' });
+    },
+  });
+
+  useEffect(() => {
+    if (!isAuth) {
+      RefreshAsync();
+    }
+  }, [isAuth, RefreshAsync]);
 
   return isAuth ? 
       <Outlet /> : 
