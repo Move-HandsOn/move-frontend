fe4874874e86673ef23e9f9adeace2c6e88b638d
fernando <anjosdelacerda@gmail.com>
2024-11-05 15:23:24 -0300

    feat: adicionando implementação do componente LISTMYGROUPS com a api

--- src/components/ListMyGroups/ListMyGroups.tsx
+++ src/components/ListMyGroups/ListMyGroups.tsx
@@ -1,6 +1,6 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import style from './ListMyGroups.module.css';
-import groupData from '../../mocks/groupData.json';
+// import groupData from '../../mocks/groupData.json';
 import RectangleGroup from '../RectangleGroup/RectangleGroup';
 import GroupCard from '../GroupCard/GroupCard';
 
@@ -8,10 +8,50 @@ type ListMyGroupsProps = React.ComponentProps<"ul"> & {
     variant: 'myGroups' | 'otherGroups' | string
 } 
 
+type GroupProps = {
+    id: number;
+    group_image: string;
+    name: string;
+    members: [];
+    group_type: string;
+    onJoin: () => void;
+    isJoined?: boolean;
+    isParticipation?: boolean;
+  };
+
 const ListMyGroups = ({variant, ...props}: ListMyGroupsProps) =>{
 
-    const [group] = useState(groupData)
-    const myGroups = group.filter((gpr) => gpr.name.split("").some((letter) => letter.toLocaleLowerCase() === "i"))
+    const [isLoading, setIsLoading] = useState(true);
+    const [groups, setGroups] = useState<GroupProps[]>([]);
+    const myGroups = groups.filter((gpr) => gpr.name.split("").some((letter) => letter.toLocaleLowerCase() === "i"))
+    const authToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im5hbmRvQG1vdmUuY29tIiwic3ViIjoiMzI2NzUwYjUtNTgzYS00NjhmLTg3ZDktOTg5NGRkOTY1NWIyIiwiaWF0IjoxNzMwODI4NzM2LCJleHAiOjE3Mzg2MDQ3MzZ9.C6A8QB5b-Q5Lfvdbck745YUc0fwbW8xvzheFt_R72Uc";
+
+
+    useEffect(() => {
+        const fetchGroups = async () => {
+            setIsLoading(true);
+            try {
+                const response = await fetch('https://move-backend-yuih.onrender.com/groups', {
+                    method: 'GET',
+                    headers: {
+                        'Authorization': `Bearer ${authToken}`
+                    }
+                });
+                if (!response.ok) {
+                    throw new Error('Erro ao buscar grupos');
+                }
+                const data = await response.json();
+                setGroups(data);
+            } catch (error) {
+                console.error(error);
+            } finally {
+                setIsLoading(false);
+            }
+        };
+        fetchGroups();
+    }, []);
+
+    console.log(groups)
 
     const handleJoinGroup = (groupId: number) => {
         console.log(`Usuário quer participar do grupo ${groupId}`);
@@ -31,20 +71,22 @@ const ListMyGroups = ({variant, ...props}: ListMyGroupsProps) =>{
                 myGroups.map((grp)=>(
                     <RectangleGroup 
                         id={grp.id}
+                        key={grp.id}
                         title={grp.name}
-                        img={grp.image}
-                        members={grp.members}
+                        img={grp.group_image}
+                        members={grp.members.length}
                         events={1}
                         />
                 ))
                 :
-                group.map((grp)=>(
+                groups.map((grp)=>(
                     <GroupCard
-                        image={grp.image}
+                        image={grp.group_image}
                         id={grp.id}
+                        key={grp.id}
                         name={grp.name}
-                        members={grp.members}
-                        privacy={grp.privacy}
+                        members={grp.members.length}
+                        privacy={grp.group_type}
                         onJoin={() => handleJoinGroup(grp.id)}
                         />
                 ))
