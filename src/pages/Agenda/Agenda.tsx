433c1d06f6fc7f5dd32324d17c028b660bacd9b3
Amandalfs <amandalfs57@gmail.com>
2024-11-19 15:26:24 -0300

    feat: criado o button de novo evento

--- src/pages/Agenda/Agenda.tsx
+++ src/pages/Agenda/Agenda.tsx
@@ -3,12 +3,18 @@ import NavigationDays from '@/components/NavigationDays/NavigationDays';
 import style from './agenda.module.css';
 import NavBar from '@/components/NavBar/NavBar';
 import TabBar from '@/components/tabBar/tabBar';
+import Button from '@/components/Button/Button';
+import plus from '@/assets/PlusWhite.png'
+import { Link } from 'react-router-dom';
 
 function Agenda() {
   return (
     <div className={style.container}>
       <NavBar title="Agenda" />
       <NavigationDays />
+      <Link to={"/new-event"}>
+        <Button variant='standard' className={style.buttonNewEvent} ><img src={plus}></img></Button>
+      </Link>
       <TabBar />
     </div>
   );
