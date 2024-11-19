68fa77f4db9169e9c167f545ff46ab9d5fad89d4
Amandalfs <amandalfs57@gmail.com>
2024-11-19 14:58:09 -0300

    feat: criado o formulario base do novo evento

--- src/pages/NewEvent/NewEvent.tsx
+++ src/pages/NewEvent/NewEvent.tsx
@@ -4,6 +4,7 @@ import Button from '@/components/Button/Button';
 import { TextArea } from '@/components/TextArea/TextArea';
 import InputStd from '@/components/InputStd/InputStd';
 import { DatePicker } from 'antd';
+import HourInput from '@/components/HourInput/HourInput';
 
 function NewEvent() {
   return (
@@ -21,17 +22,24 @@ function NewEvent() {
             <InputStd style={{ width: "100%"}} placeholder='Local (Escreva a Localização)' />
             <DatePicker
                 placeholder='Quando?'
+                className={style.datepicker}
             />
             <div className={style.inputs_durations}>
-              
+                <HourInput titleDefault={"Inicio"} dates={[{ label: "Inicio", value: 0}]} value={0} />
+                <HourInput titleDefault={"Fim"} dates={[{ label: "Fim", value: 0}]} value={0}  />
+            </div>
+            <h3>Compartilhamento(Opcional)</h3>
+            <div className={style.flex_row_gap_12}>
+                <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike" />
+                <p>Compartilhar evento no meu perfil</p>
+            </div>
+            <div className={style.flex_row_gap_12}>
+                <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike" />
+                <p>Adicionar evento em um grupo</p>
             </div>
           </div>
-
-          <div className={style.group}>
-            <h3>Compartilhamento(Opcional)</h3>
-          </div>
         </div>
-        <Button variant="gray" name="Salvar" type='submit'/>
+        <Button variant="standard" name="Salvar" type='submit'/>
       </form>
     </div>
   );
