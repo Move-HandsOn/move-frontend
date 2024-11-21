05bb25c818a63762e05ed96afc3477e7774b7360
Amandalfs <amandalfs57@gmail.com>
2024-11-21 10:57:57 -0300

    fix: arrumado o css do button de novo evento

--- src/pages/Agenda/Agenda.tsx
+++ src/pages/Agenda/Agenda.tsx
@@ -4,7 +4,6 @@ import style from './agenda.module.css';
 import NavBar from '@/components/NavBar/NavBar';
 import TabBar from '@/components/tabBar/tabBar';
 import Button from '@/components/Button/Button';
-import plus from '@/assets/PlusWhite.png'
 import { Link } from 'react-router-dom';
 
 function Agenda() {
@@ -13,7 +12,7 @@ function Agenda() {
       <NavBar title="Agenda" />
       <NavigationDays />
       <Link to={"/new-event"}>
-        <Button variant='standard' className={style.buttonNewEvent} ><img src={plus}></img></Button>
+        <Button variant='standard' className={style.buttonNewEvent}><p>+</p></Button>
       </Link>
       <TabBar />
     </div>
