639092b86e82ca5bf41673678da21021e4d51ab0
Daniel <danieldeandradelopes@gmail.com>
2024-11-25 10:53:34 -0300

    fix comments

--- src/routes/MainRoutes.tsx
+++ src/routes/MainRoutes.tsx
@@ -1,20 +1,20 @@
 import FeedLayout from '@/FeedLayout.tsx';
+import Agenda from '@/pages/Agenda/Agenda.tsx';
 import Group from '@/pages/Group/Group.tsx';
 import Groups from '@/pages/Groups/Groups.tsx';
+import NewEvent from '@/pages/NewEvent/NewEvent.tsx';
+import NewGroup from '@/pages/NewGroup/NewGroup.tsx';
 import Profile from '@/pages/Profile/Profile.tsx';
 import SearchPage from '@/pages/SearchPage/SearchPage.tsx';
 import ProfileLayout from '@/ProfileLayout.tsx';
+import { Providers } from '@/Provider/index.tsx';
 import { Route, Routes } from 'react-router-dom';
 import App from '../app.tsx';
 import Feed from '../pages/Feed/Feed';
 import NewActivity from '../pages/NewActivity/NewActivity.tsx';
-import RequireAuth from './RequireAuth';
-import { Providers } from '@/Provider/index.tsx';
-import NewGroup from '@/pages/NewGroup/NewGroup.tsx';
-import Agenda from '@/pages/Agenda/Agenda.tsx';
-import NewEvent from '@/pages/NewEvent/NewEvent.tsx';
-import IsPublic from './IsPublic.jsx';
 import Notifications from '../pages/Notifications/Notifications.tsx';
+import IsPublic from './IsPublic.jsx';
+import RequireAuth from './RequireAuth';
 
 function MainRoutes() {
   return (
@@ -24,7 +24,7 @@ function MainRoutes() {
           <Route path="/" element={<App />} />
         </Route>
 
-        {/* <Route element={<RequireAuth />}> */}
+        <Route element={<RequireAuth />}>
           <Route
             path="/feed"
             element={
@@ -35,7 +35,7 @@ function MainRoutes() {
           />
           <Route path="/new-activity" element={<NewActivity />} />
           <Route path="/new-group" element={<NewGroup />} />
-          <Route path="/new-event" element={<NewEvent /> }/>
+          <Route path="/new-event" element={<NewEvent />} />
           <Route path="/search" element={<SearchPage />} />
           <Route
             path="/profile"
@@ -50,7 +50,7 @@ function MainRoutes() {
           <Route path="/group" element={<Group />} />
           <Route path="/schedule" element={<Agenda />} />
         </Route>
-      {/* </Route> */}
+      </Route>
     </Routes>
   );
 }
