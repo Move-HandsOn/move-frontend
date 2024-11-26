a3c6e606eb23fc564b570462616074abaccf5b73
Isabella Brito <isadocarmo7@gmail.com>
2024-11-26 14:43:03 -0300

    removing buttons image background on forms

--- src/components/UploadAll/UploadAll.tsx
+++ src/components/UploadAll/UploadAll.tsx
@@ -2,7 +2,7 @@ import trash from '@/assets/Trash-1.svg';
 import type { UploadFile, UploadProps } from 'antd';
 import { Upload } from 'antd';
 import styles from './UploadAll.module.css';
-import camera from "@/assets/CameraPlus.svg"
+import camera from '@/assets/CameraPlus.svg';
 import Button from '../Button/Button';
 
 type UploadAllProps = {
@@ -21,42 +21,52 @@ export function UploadAll({ fileList, setFileList }: UploadAllProps) {
     setFileList(fileList.filter((item) => item.uid !== file.uid));
   };
 
-  const uploadButton = <Button type="button" variant='gray'><img src={camera} alt="" />Adicionar Foto</Button>;
+  const uploadButton = (
+    <Button type="button" variant="gray">
+      <img className={styles.uploadIcon} src={camera} alt="" />
+      Adicionar Foto
+    </Button>
+  );
 
   return (
     <div className={styles.container}>
-     {fileList.length > 0 && <div className={styles.containerCards}>
-        {fileList.map((file) => (
-          <div key={file.uid} className={styles.customCard}>
-            {file.url || file.originFileObj ? (
-              <img
-              src={
-                  file.url ||
-                  (file.originFileObj && URL.createObjectURL(file.originFileObj))
-                }
-                alt={file.name}
-              />
-            ) : (
-              <span>{file.name}</span>
-            )}
+      {fileList.length > 0 && (
+        <div className={styles.containerCards}>
+          {fileList.map((file) => (
+            <div key={file.uid} className={styles.customCard}>
+              {file.url || file.originFileObj ? (
+                <img
+                  src={
+                    file.url ||
+                    (file.originFileObj &&
+                      URL.createObjectURL(file.originFileObj))
+                  }
+                  alt={file.name}
+                />
+              ) : (
+                <span>{file.name}</span>
+              )}
 
-            <button
-              className={styles.customDeleteButton}
-              onClick={() => handleRemove(file)}
+              <button
+                className={styles.customDeleteButton}
+                onClick={() => handleRemove(file)}
               >
-              <img src={trash} className={styles.deleteIcon} />
-            </button>
-          </div>
-        ))}
-      </div>}
-     {fileList.length < uploadLimited ? <Upload
-        onChange={handleChange}
-        showUploadList={false}
-        accept='images/*'
-        beforeUpload={() => false}
-        > 
-        {uploadButton}
-      </Upload> : null}
+                <img src={trash} className={styles.deleteIcon} />
+              </button>
+            </div>
+          ))}
+        </div>
+      )}
+      {fileList.length < uploadLimited ? (
+        <Upload
+          onChange={handleChange}
+          showUploadList={false}
+          accept="images/*"
+          beforeUpload={() => false}
+        >
+          {uploadButton}
+        </Upload>
+      ) : null}
     </div>
   );
 }
--- src/pages/NewGroup/NewGroup.tsx
+++ src/pages/NewGroup/NewGroup.tsx
@@ -6,48 +6,47 @@ import NavBar from '@/components/NavBar/NavBar';
 import Button from '@/components/Button/Button';
 import { TextArea } from '@/components/TextArea/TextArea';
 import { UploadAll } from '../../components/UploadAll/UploadAll';
-import * as zod from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
+import * as zod from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import { UploadFile } from 'antd';
 import InputStd from './../../components/InputStd/InputStd';
-import friendIcon from "@/assets/friends.png";
+import friendIcon from '@/assets/friends.png';
 import ModalSelectFriend from '@/components/ModalSelectFriend/ModalSelectFriend';
-import { useMutation} from '@tanstack/react-query';
+import { useMutation } from '@tanstack/react-query';
 import { NewGroupRequest } from '@/services/requests';
 import { useNavigate } from 'react-router-dom';
 
 const activitiesDone: Array<string> = [
-  "Corrida",
-  "Caminhada",
-  "Ciclismo",
-  "Trilha",
-  "Futebol",
-  "Basquete",
-  "Vôlei",
-  "Tênis",
-  "Natação",
-  "Musculação",
-  "Crossfit"
+  'Corrida',
+  'Caminhada',
+  'Ciclismo',
+  'Trilha',
+  'Futebol',
+  'Basquete',
+  'Vôlei',
+  'Tênis',
+  'Natação',
+  'Musculação',
+  'Crossfit',
 ];
-
 
 type Friend = {
   idFriend: string;
   name: string;
   image: string;
-}
-
+};
 
 function NewGroup() {
-  const navigate =  useNavigate();
+  const navigate = useNavigate();
 
- 
   const [fileList, setFileList] = useState<UploadFile[]>([]);
-  
+
   const [selectedOption, setSelectedOption] = useState('');
 
-  const [selectFriends, setSelectFriends] = useState<{ [key: string]: Friend }>({});
+  const [selectFriends, setSelectFriends] = useState<{ [key: string]: Friend }>(
+    {}
+  );
   const [modalSelectFriends, setModalSelectFriends] = useState<boolean>(false);
 
   const addFriend = (newFriend: Friend) => {
@@ -58,126 +57,153 @@ function NewGroup() {
       return prevFriends;
     });
   };
-  
 
   const [options] = useState<Array<string>>([
     'Publicar no grupo',
     'Publicar no grupo e no perfil',
-  ])
+  ]);
 
-
-  const handleSelectPublishChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+  const handleSelectPublishChange = (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => {
     const option = event.target.value;
     setSelectedOption(option);
-  }
+  };
 
   const dataGroupValidSchema = zod.object({
-    group_type: zod.enum([
-      'Publicar no grupo',
-      'Publicar no grupo e no perfil',
-    ]).refine(val => ['Publicar no grupo', 'Publicar no grupo e no perfil'].includes(val), {
-      message: 'Tipo de post inválido. Escolha entre "Publicar no grupo" ou "Publicar no grupo e no perfil".',
-    }),
+    group_type: zod
+      .enum(['Publicar no grupo', 'Publicar no grupo e no perfil'])
+      .refine(
+        (val) =>
+          ['Publicar no grupo', 'Publicar no grupo e no perfil'].includes(val),
+        {
+          message:
+            'Tipo de post inválido. Escolha entre "Publicar no grupo" ou "Publicar no grupo e no perfil".',
+        }
+      ),
     category_name: zod.enum([
-      "Corrida",
-      "Caminhada",
-      "Ciclismo",
-      "Trilha",
-      "Futebol",
-      "Basquete",
-      "Vôlei",
-      "Tênis",
-      "Natação",
-      "Musculação",
-      "Crossfit"
+      'Corrida',
+      'Caminhada',
+      'Ciclismo',
+      'Trilha',
+      'Futebol',
+      'Basquete',
+      'Vôlei',
+      'Tênis',
+      'Natação',
+      'Musculação',
+      'Crossfit',
     ]),
     description: zod.string().optional(),
     name: zod.string().min(3, 'O nome do grupo é obrigatório.'),
     files: zod.array(zod.instanceof(File)).optional(),
   });
-  
+
   type IDataGroupValidSchema = zod.infer<typeof dataGroupValidSchema>;
-  
-  const { register, handleSubmit, formState: { isValid }} = useForm<IDataGroupValidSchema>({
+
+  const {
+    register,
+    handleSubmit,
+    formState: { isValid },
+  } = useForm<IDataGroupValidSchema>({
     resolver: zodResolver(dataGroupValidSchema),
     defaultValues: {
-      group_type: "Publicar no grupo",
-      description: "",
-      files: []
-    }
+      group_type: 'Publicar no grupo',
+      description: '',
+      files: [],
+    },
   });
 
   const { mutateAsync: FormAsync } = useMutation({
     mutationFn: async (data: IDataGroupValidSchema) => {
-      const friendIds = Object.keys(selectFriends).length > 0 
-      ? Object.values(selectFriends).map(friend => friend.idFriend) 
-      : [];
+      const friendIds =
+        Object.keys(selectFriends).length > 0
+          ? Object.values(selectFriends).map((friend) => friend.idFriend)
+          : [];
       await NewGroupRequest({
         ...data,
-        friend_ids: friendIds
-      });  
+        friend_ids: friendIds,
+      });
     },
-    onSuccess: ()=>{
-      navigate("/groups");
-    }
-  })
+    onSuccess: () => {
+      navigate('/groups');
+    },
+  });
 
-
-   
   const onSubmit = (data: IDataGroupValidSchema) => {
     FormAsync(data);
-  }
+  };
 
   return (
     <div className={style.container}>
       <NavBar title="Novo Grupo" />
-      <form  onSubmit={handleSubmit(onSubmit)} className={style.formContainer}>
+      <form onSubmit={handleSubmit(onSubmit)} className={style.formContainer}>
         <div className={style.register}>
           <div className={style.group}>
             <h3>Detalhes da atividade</h3>
             <PublishOptionsList
               options={options}
               value={selectedOption}
-              {...register("group_type")}
+              {...register('group_type')}
               onChange={handleSelectPublishChange}
             />
             <ActivityList
               options={activitiesDone}
-              {...register("category_name")}
+              {...register('category_name')}
             />
-            <InputStd style={{ width: "100%"}} placeholder='Qual o nome do grupo?' {...register("name")} />
+            <InputStd
+              style={{ width: '100%' }}
+              placeholder="Qual o nome do grupo?"
+              {...register('name')}
+            />
           </div>
 
           <div className={style.group}>
             <h3>Nos conte um pouco mais</h3>
-            <TextArea 
-              id="activity-details" 
+            <TextArea
+              id="activity-details"
               placeholder="Detalhes da minha atividade (Opcional)"
-              {...register("description")}
+              {...register('description')}
             ></TextArea>
             <UploadAll fileList={fileList} setFileList={setFileList} />
-            <Button variant='gray' onClick={()=> { setModalSelectFriends(true)}}> 
-              <img src={friendIcon} alt="adicionar amigo" />
+            <Button
+              variant="gray"
+              onClick={() => {
+                setModalSelectFriends(true);
+              }}
+            >
+              <img
+                src={friendIcon}
+                className={style.friendIcon}
+                alt="adicionar amigo"
+              />
               Adicionar Amigos
             </Button>
-            {Object.values(selectFriends).map(({idFriend, image, name}: Friend) => (
-                <div  className={style.friend} key={idFriend}>
-                <img src={image} alt="" />
-                <h1>{name}</h1>
-            </div>
-            ))}
+            {Object.values(selectFriends).map(
+              ({ idFriend, image, name }: Friend) => (
+                <div className={style.friend} key={idFriend}>
+                  <img src={image} alt="" />
+                  <h1>{name}</h1>
+                </div>
+              )
+            )}
           </div>
         </div>
-        <Button variant="gray" disabled={!isValid} name="Publicar" type='submit'/>
+        <Button
+          variant="gray"
+          disabled={!isValid}
+          name="Publicar"
+          type="submit"
+        />
       </form>
-      {
-        modalSelectFriends && <ModalSelectFriend 
-          closeModal={()=> { setModalSelectFriends(false)}}
-          handleGroup={addFriend} 
-        >
-
-        </ModalSelectFriend>
-      }
+      {modalSelectFriends && (
+        <ModalSelectFriend
+          closeModal={() => {
+            setModalSelectFriends(false);
+          }}
+          handleGroup={addFriend}
+        ></ModalSelectFriend>
+      )}
     </div>
   );
 }
