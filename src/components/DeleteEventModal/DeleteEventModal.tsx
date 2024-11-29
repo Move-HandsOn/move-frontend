0a06dc3c0a22431d7c994beb667bc2c979628e59
Amandalfs <amandalfs57@gmail.com>
2024-11-29 10:02:44 -0300

    feat: criado o componente de excluir evento

---
+++ src/components/DeleteEventModal/DeleteEventModal.tsx
@@ -0,0 +1,62 @@
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Button from '../Button/Button';
+import styles from './DeleteEventModal.module.css';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 300,
+  height: 182,
+  bgcolor: 'var(--WHITE)',
+  border: 'none',
+  borderRadius: '15px',
+  boxShadow: 24,
+  p: 4,
+};
+
+type Props = {
+  open: boolean;
+  onClose: () => void;
+  onDelete: () => void;
+};
+
+export default function DeleteEventModal({ open, onClose, onDelete }: Props) {
+  return (
+    <div>
+      <Modal
+        open={open}
+        onClose={onClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+        closeAfterTransition
+        sx={{
+          backgroundColor: 'rgba(0, 0, 0, 0.25)',
+        }}
+      >
+        <Box sx={style}>
+          <h3 className={styles.exclusionTitle}>Excluir evento</h3>
+          <p className={styles.exclusionText}>
+            Tem certeza de que deseja excluir este evento?
+          </p>
+          <div className={styles.exclusionBtnBox}>
+            <Button
+              variant="gray"
+              name="Cancelar"
+              style={{ width: '115px', background: 'none' }}
+              onClick={onClose}
+            />
+            <Button
+              variant="standard"
+              name="Excluir"
+              style={{ width: '115px' }}
+              onClick={onDelete}
+            />
+          </div>
+        </Box>
+      </Modal>
+    </div>
+  );
+}
